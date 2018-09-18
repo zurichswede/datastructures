226ce8323b70e87238118b89a11de0ebd6e89969
rbzurich <rickardonipad@gmail.com>
2018-09-18 07:08:33 +0200

    test class

---
+++ support classes/FamilyMember.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace googlepractice1
+{
+    public class FamilyMember : IComparable
+    {
+        public int Age { get; private set; }
+        public string Name { get; private set; } 
+
+        public FamilyMember(string name, int age)
+        {
+            this.Age = age;
+            this.Name = name;
+        }
+
+        public override string ToString()
+        {
+            string result = "Name: " + this.Name + " Age: " + this.Age.ToString();
+            return result;
+        }
+
+
+        public int CompareTo(object obj)
+        {
+            FamilyMember tmp = (FamilyMember)obj;
+            return Age.CompareTo(tmp.Age);
+        }
+    }
+}
